9dadd4e287a9c6163259e1ad059c225bef8aed78
sahargholami1992 <sahar_gholami03@gmail.com>
2024-01-29 17:21:03 +0330

    Initial commit

---
+++ src/main/java/com/example/final_project_3/FinalProject3Application.java
@@ -0,0 +1,13 @@
+package com.example.final_project_3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalProject3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinalProject3Application.class, args);
+    }
+
+}
